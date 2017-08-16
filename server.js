6130a9f2847e00ef7289f8878568a6f8ab503878
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-15 21:10:26 -0600

    Add post route for folders on server side.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Jet Fuel'
+app.locals.folders = {}
 
 app.use(express.static('public'));
 
@@ -13,6 +14,14 @@ app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html')
 });
 
+app.post('/api/v1/folders', (request, response) => {
+  const { folder } = request.body;
+
+  app.locals.folders[folder] = folder;
+
+  response.status(201).json({ folder });
+});
+
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost${app.get('port')}.`)
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
