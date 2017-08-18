3b1af16f172f32025251f517fd6fc5f72a1b2b94
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-17 21:36:12 -0600

    Add post new link from client side and prepend to DOM.

--- public/scripts.js
+++ public/scripts.js
@@ -6,7 +6,11 @@ $(document).ready(() => {
 
 const displayFolders = (data) => {
   data.forEach(folder => {
-    $('#folder-select').append(`<option id=${folder.id} value=${folder.id}>${folder.name}</option>`)
+    $('#folder-select').append(`
+      <option id=${folder.id} value=${folder.id}>
+        ${folder.name}
+      </option>
+    `)
   })
 }
 
@@ -29,7 +33,7 @@ const prependNewLink = (event) => {
   $('#user-links').prepend(`<a href="#">${userInputs.name}</a>`)
 }
 
-$('#new-link-submit').click(prependNewLink);
+// $('#new-link-submit').click(prependNewLink);
 
 const getNewFolderName = () => {
   const nameValue = $('#folder-name').val();
@@ -65,6 +69,10 @@ const getFolderVal = () => {
 const handleFolderChange = () => {
   const folderId = getFolderVal()
 
+  if (folderId === 0) {
+    return
+  }
+
   fetch(`/api/v1/folders/${folderId}/links`)
     .then((response) => response.json())
     .then((data) => {
@@ -84,3 +92,30 @@ const handleFolderChange = () => {
 
 
 $('#folder-select').change(handleFolderChange)
+$('#new-link-submit').click(createLink)
+
+function createLink() {
+  const folderId = getFolderVal();
+  const linkInputs = getNewLinkInputs();
+
+  fetch('/api/v1/links', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      long_URL: linkInputs.link,
+      description: linkInputs.name,
+      folder_id: folderId
+      })
+    })
+    .then(response => response.json())
+    .then(link => {
+      $('#user-links').prepend(`
+          <div class="card">
+            <p>${link.description}</p>
+            <p>${link.short_URL}</p>
+            <p>${link.created_at}</p>
+          </div>
+        `);
+    })
+    .catch(error => console.log(error))
+}
